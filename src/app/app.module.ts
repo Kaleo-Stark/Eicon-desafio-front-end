21cc3ee21dfaef4143a0ef8963bb588f05b04025
Kaleo-Stark <kaleovieira.dev@gmail.com>
2020-11-17 20:12:42 -0300

    Estilização pagina inicial, criação e estilização do componente card-filme.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardFilmeComponent } from './componentes/card-filme/card-filme.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardFilmeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/componentes/card-filme/card-filme.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-filme',
+  templateUrl: './card-filme.component.html',
+  styleUrls: ['./card-filme.component.css']
+})
+export class CardFilmeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
