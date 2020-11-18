0d6d2811ada234e03f9a5a052e5592584ca1e02d
Kaleo-Stark <kaleovieira.dev@gmail.com>
2020-11-17 23:37:00 -0300

    Passagem de dados da lista de filme para o card de filme.

--- src/app/componentes/card-filme/card-filme.component.ts
+++ src/app/componentes/card-filme/card-filme.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card-filme',
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardFilmeComponent implements OnInit {
 
+  @Input() dadosFilme: {
+    id: 0,
+    title: '',
+    release_date: '',
+    poster_path: ''
+  }; 
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public formatarData = (data) => {
+    return data;
   }
 
 }
